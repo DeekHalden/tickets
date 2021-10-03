26ec3bc443a80cac3d1fc4b2b947eb57f79fcb38
deekhalden <deekhalden@gmail.com>
2021-10-03 15:18:37 +0300

    expiration service added

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,35 @@
+import {
+  Listener,
+  OrderCreatedEvent,
+  Subjects,
+} from '@microservices-tessera/common'
+
+import { Message } from 'node-nats-streaming'
+
+import { expirationQueue } from '../../queues/expiration-queue'
+
+import { queueGroupName } from './queue-group-name'
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated
+  queueGroupName: string = queueGroupName
+
+  async onMessage(
+    data: OrderCreatedEvent['data'],
+    msg: Message
+  ): Promise<void> {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime()
+    console.log(delay)
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay,
+      }
+    )
+      
+    msg.ack()
+  }
+}
