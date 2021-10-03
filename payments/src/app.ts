cea69a3cb067b453f64120a526f7028a46fac0cb
deekhalden <deekhalden@gmail.com>
2021-10-03 16:32:13 +0300

    initial service cleanup

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -2,17 +2,11 @@ import express from 'express'
 import 'express-async-errors'
 import cookieSession from 'cookie-session'
 
-
 import {
   currentUser,
   errorHandler,
   NotFoundError,
 } from '@microservices-tessera/common'
-
-import { createTicketRouter } from './routes/new'
-import { showTicketRouter } from './routes/show'
-import { indexRouter } from './routes'
-import { updateTicketRouter } from './routes/update'
 
 const app = express()
 app.set('trust proxy', true)
@@ -26,10 +20,6 @@ app.use(
 )
 
 app.use(currentUser)
-app.use(createTicketRouter)
-app.use(showTicketRouter)
-app.use(indexRouter)
-app.use(updateTicketRouter)
 
 app.all('*', async () => {
   throw new NotFoundError()
