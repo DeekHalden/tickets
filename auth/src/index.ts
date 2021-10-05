79842d25b485a27859dee479cf234d24e3830cf0
DeekHalden <DeekHalden@users.noreply.github.com>
2021-10-05 21:01:47 +0300

    Update index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import { app } from './app'
 
 const start = async () => {
-  console.log('starting!!!...')
+  console.log('starting!!!!!...')
   if (!process.env.JWT_KEY) {
     throw new Error('Please assign a JWT_KEY variable')
   }
